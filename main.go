b3351442d0ca55592a0c9a654c24e03434d6c211
GyeongSu Han <micalgenus@gmail.com>
2023-03-12 23:30:35 +0900

    Add default handler

--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
-    "net/http"
+  "encoding/json"
+  "io/ioutil"
+  "net/http"
 )
 
-func HealthHandler(w http.ResponseWriter, req *http.Request) {
+type response struct {
+  URL     string      "json:\"url\""
+  Method  string      "json:\"method\""
+  Headers http.Header "json:\"headers\""
+  Body    []byte      "json:\"body\""
+}
+
+func healthHandler(w http.ResponseWriter, req *http.Request) {
   w.Write([]byte("OK"))
 }
 
+func echoHandler(w http.ResponseWriter, req *http.Request) {
+  var err error
+  res := &request{}
+  res.Method = req.Method
+  res.Headers = req.Header
+  res.URL = req.URL.String()
+  res.Body, err = ioutil.ReadAll(req.Body)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  response, err := json.MarshalIndent(res, "", "  ")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(response)
+}
+
 func main() {
-  http.HandleFunc("/health", HealthHandler)
+  http.HandleFunc("/health", healthHandler)
+  http.HandleFunc("/", echoHandler)
 
   http.ListenAndServe(":8080", nil)
 }
