e9016b3ebb02b7e38be756e402be8b7dcf649f13
GyeongSu Han <micalgenus@gmail.com>
2023-03-12 23:33:51 +0900

    Add error handler

--- main.go
+++ main.go
@@ -17,6 +17,14 @@ func healthHandler(w http.ResponseWriter, req *http.Request) {
   w.Write([]byte("OK"))
 }
 
+func notFoundHandler(w http.ResponseWriter, req *http.Request) {
+  w.WriteHeader(404)
+}
+
+func errorHandler(w http.ResponseWriter, req *http.Request) {
+  w.WriteHeader(500)
+}
+
 func echoHandler(w http.ResponseWriter, req *http.Request) {
   var err error
   res := &request{}
@@ -41,6 +49,8 @@ func echoHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
   http.HandleFunc("/health", healthHandler)
+  http.HandleFunc("/notfound", notFoundHandler)
+  http.HandleFunc("/error", errorHandler)
   http.HandleFunc("/", echoHandler)
 
   http.ListenAndServe(":8080", nil)
